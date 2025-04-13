152e381503871cc959d9e36dbe2b6ac28fad12c9
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-13 13:48:18 +0300

    working

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,7 +14,7 @@ class ProductsController extends Controller
     public function index()
     {
         $query = Producs::query();
-        $products = $query->paginate(10)->onEachPage(1);
+        $products = $query->paginate(10)->onEachSide(1);
         return inertia('Products/Index',[]);
     }
 
